013bc148d4b3f10c72f42908e58b8eb85772b9d3
schedin <schedin@users.noreply.github.com>
2023-12-12 15:53:39 +0100

    [MSHARED-1344] Support for tsapolicyid and tsadigestalg
    Adding support for -tsapolicyid and -tsadigestalg

--- src/main/java/org/apache/maven/shared/jarsigner/JarSignerSignRequest.java
+++ src/main/java/org/apache/maven/shared/jarsigner/JarSignerSignRequest.java
@@ -29,34 +29,55 @@ import java.io.File;
 public class JarSignerSignRequest extends AbstractJarSignerRequest {
 
     /**
-     * See <a href="http://docs.oracle.com/javase/6/docs/technotes/tools/windows/jarsigner.html#Options">options</a>.
+     * See <a href="https://docs.oracle.com/javase/8/docs/technotes/tools/windows/jarsigner.html#CCHIFIAD">options</a>.
      */
     private String keypass;
 
     /**
-     * See <a href="http://docs.oracle.com/javase/6/docs/technotes/tools/windows/jarsigner.html#Options">options</a>.
+     * See <a href="https://docs.oracle.com/javase/8/docs/technotes/tools/windows/jarsigner.html#CCHIFIAD">options</a>.
      */
     private String sigfile;
 
     /**
-     * See <a href="http://docs.oracle.com/javase/6/docs/technotes/tools/windows/jarsigner.html#Options">options</a>.
+     * The {@code -tsa} parameter, the URL to the Time Stamping Authority (TSA) server.
+     * See <a href="https://docs.oracle.com/javase/8/docs/technotes/tools/windows/jarsigner.html#CCHIFIAD">options</a>.
      */
     private String tsaLocation;
 
     /**
-     * See <a href="http://docs.oracle.com/javase/6/docs/technotes/tools/windows/jarsigner.html#Options">options</a>.
+     * The {@code -tsacert} parameter. Alias for a certificate in the active keystore. From the certificate the X509v3
+     * extension "Subject Information Access" field is examined to find a TSA server URL.
+     * See <a href="https://docs.oracle.com/javase/8/docs/technotes/tools/windows/jarsigner.html#CCHIFIAD">options</a>.
      */
     private String tsaAlias;
 
     /**
-     * See <a href="http://docs.oracle.com/javase/6/docs/technotes/tools/windows/jarsigner.html#Options">options</a>.
+     * The {@code -tsapolicyid} parameter, an OID to send to the TSA server to identify the policy ID the server should
+     * use. See
+     * <a href="https://docs.oracle.com/javase/8/docs/technotes/tools/windows/jarsigner.html#CCHIFIAD">options</a>.
+     *
+     * @since 3.1.0
+     */
+    private String tsapolicyid;
+
+    /**
+     * The {@code -tsadigestalg} parameter, the message digest algorithm for TSA server to use. Only available in Java
+     * 11 and later. See
+     * <a href="https://docs.oracle.com/en/java/javase/11/tools/jarsigner.html">options</a>.
+     *
+     * @since 3.1.0
+     */
+    private String tsadigestalg;
+
+    /**
+     * See <a href="https://docs.oracle.com/javase/8/docs/technotes/tools/windows/jarsigner.html#CCHIFIAD">options</a>.
      */
     protected File signedjar;
 
     /**
      * Location of the extra certchain file to be used during signing.
      *
-     * See <a href="http://docs.oracle.com/javase/7/docs/technotes/tools/windows/jarsigner.html#Options">options</a>.
+     * See <a href="https://docs.oracle.com/javase/8/docs/technotes/tools/windows/jarsigner.html#CCHIFIAD">options</a>.
      * @since 3.0.0
      */
     protected File certchain;
@@ -77,6 +98,14 @@ public class JarSignerSignRequest extends AbstractJarSignerRequest {
         return tsaAlias;
     }
 
+    public String getTsapolicyid() {
+        return tsapolicyid;
+    }
+
+    public String getTsadigestalg() {
+        return tsadigestalg;
+    }
+
     public void setKeypass(String keypass) {
         this.keypass = keypass;
     }
@@ -91,6 +120,14 @@ public class JarSignerSignRequest extends AbstractJarSignerRequest {
 
     public void setTsaAlias(String tsaAlias) {
         this.tsaAlias = tsaAlias;
+    }
+
+    public void setTsapolicyid(String tsapolicyid) {
+        this.tsapolicyid = tsapolicyid;
+    }
+
+    public void setTsadigestalg(String tsadigestalg) {
+        this.tsadigestalg = tsadigestalg;
     }
 
     public File getSignedjar() {
